3dc383f584c62a3c3fe4bd3405609ed1f128a9aa
Austin Auger <a.ustin.mark.auger@gmail.com>
2021-12-09 11:06:37 -0500

    Create LambdaConfigurator.cs
    Added LambdaConfigurator so filters can internally initialize and apply other filters if they so choose. This will allow the creation of filter pipelines.

---
+++ AAImageFilter/LibraryConfigurators/LambdaConfigurator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AAImageFilter.Interfaces;
+
+namespace AAImageFilter.LibraryConfigurators
+{
+    public class LambdaConfigurator<T> : IPluginConfigurator<T>
+    {
+        private readonly Func<T> _lambda;
+
+        public LambdaConfigurator(Func<T> lambda)
+        {
+            _lambda = lambda;
+        }
+
+        public T GetPluginConfiguration()
+        {
+            return _lambda();
+        }
+    }
+}
