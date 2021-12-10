3d8473635cda967f506452dabdd872946e73fc57
Austin Auger <a.ustin.mark.auger@gmail.com>
2021-12-10 09:59:45 -0500

    Made some fun tweaks to the test application

--- NET6ImageFilter/MainForm.Designer.cs
+++ NET6ImageFilter/MainForm.Designer.cs
@@ -45,40 +45,46 @@
             // 
             this.imageViewer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.imageViewer.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.imageViewer.Location = new System.Drawing.Point(0, 29);
+            this.imageViewer.Location = new System.Drawing.Point(0, 36);
             this.imageViewer.Name = "imageViewer";
-            this.imageViewer.Size = new System.Drawing.Size(800, 421);
+            this.imageViewer.Size = new System.Drawing.Size(800, 414);
             this.imageViewer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.imageViewer.TabIndex = 0;
             this.imageViewer.TabStop = false;
             // 
             // loadButton
             // 
+            this.loadButton.Image = ((System.Drawing.Image)(resources.GetObject("loadButton.Image")));
             this.loadButton.Location = new System.Drawing.Point(3, 3);
             this.loadButton.Name = "loadButton";
-            this.loadButton.Size = new System.Drawing.Size(75, 23);
+            this.loadButton.Size = new System.Drawing.Size(58, 30);
             this.loadButton.TabIndex = 1;
             this.loadButton.Text = "Load";
+            this.loadButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.loadButton.UseVisualStyleBackColor = true;
             this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
             // 
             // saveButton
             // 
-            this.saveButton.Location = new System.Drawing.Point(84, 3);
+            this.saveButton.Image = ((System.Drawing.Image)(resources.GetObject("saveButton.Image")));
+            this.saveButton.Location = new System.Drawing.Point(67, 3);
             this.saveButton.Name = "saveButton";
-            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.Size = new System.Drawing.Size(57, 30);
             this.saveButton.TabIndex = 1;
             this.saveButton.Text = "Save";
+            this.saveButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.saveButton.UseVisualStyleBackColor = true;
             this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
             // 
             // applyFilterButton
             // 
-            this.applyFilterButton.Location = new System.Drawing.Point(165, 3);
+            this.applyFilterButton.Image = ((System.Drawing.Image)(resources.GetObject("applyFilterButton.Image")));
+            this.applyFilterButton.Location = new System.Drawing.Point(130, 3);
             this.applyFilterButton.Name = "applyFilterButton";
-            this.applyFilterButton.Size = new System.Drawing.Size(75, 23);
+            this.applyFilterButton.Size = new System.Drawing.Size(59, 30);
             this.applyFilterButton.TabIndex = 1;
-            this.applyFilterButton.Text = "Apply Filter";
+            this.applyFilterButton.Text = "Filter";
+            this.applyFilterButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.applyFilterButton.UseVisualStyleBackColor = true;
             this.applyFilterButton.Click += new System.EventHandler(this.applyFilterButton_Click);
             // 
@@ -94,36 +100,42 @@
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(800, 29);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(800, 36);
             this.flowLayoutPanel1.TabIndex = 2;
             // 
             // useGeneratorButton
             // 
-            this.useGeneratorButton.Location = new System.Drawing.Point(246, 3);
+            this.useGeneratorButton.Image = ((System.Drawing.Image)(resources.GetObject("useGeneratorButton.Image")));
+            this.useGeneratorButton.Location = new System.Drawing.Point(195, 3);
             this.useGeneratorButton.Name = "useGeneratorButton";
-            this.useGeneratorButton.Size = new System.Drawing.Size(99, 23);
+            this.useGeneratorButton.Size = new System.Drawing.Size(78, 30);
             this.useGeneratorButton.TabIndex = 2;
-            this.useGeneratorButton.Text = "Use Generator";
+            this.useGeneratorButton.Text = "Generate";
+            this.useGeneratorButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.useGeneratorButton.UseVisualStyleBackColor = true;
             this.useGeneratorButton.Click += new System.EventHandler(this.useGeneratorButton_Click);
             // 
             // copyToClipboardButton
             // 
-            this.copyToClipboardButton.Location = new System.Drawing.Point(351, 3);
+            this.copyToClipboardButton.Image = ((System.Drawing.Image)(resources.GetObject("copyToClipboardButton.Image")));
+            this.copyToClipboardButton.Location = new System.Drawing.Point(279, 3);
             this.copyToClipboardButton.Name = "copyToClipboardButton";
-            this.copyToClipboardButton.Size = new System.Drawing.Size(113, 23);
+            this.copyToClipboardButton.Size = new System.Drawing.Size(59, 30);
             this.copyToClipboardButton.TabIndex = 3;
-            this.copyToClipboardButton.Text = "Copy To Clipboard";
+            this.copyToClipboardButton.Text = "Copy";
+            this.copyToClipboardButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.copyToClipboardButton.UseVisualStyleBackColor = true;
             this.copyToClipboardButton.Click += new System.EventHandler(this.copyToClipboardButton_Click);
             // 
             // fromClipboardButton
             // 
-            this.fromClipboardButton.Location = new System.Drawing.Point(470, 3);
+            this.fromClipboardButton.Image = ((System.Drawing.Image)(resources.GetObject("fromClipboardButton.Image")));
+            this.fromClipboardButton.Location = new System.Drawing.Point(344, 3);
             this.fromClipboardButton.Name = "fromClipboardButton";
-            this.fromClipboardButton.Size = new System.Drawing.Size(142, 23);
+            this.fromClipboardButton.Size = new System.Drawing.Size(61, 30);
             this.fromClipboardButton.TabIndex = 4;
-            this.fromClipboardButton.Text = "Image From Clipboard";
+            this.fromClipboardButton.Text = "Paste";
+            this.fromClipboardButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.fromClipboardButton.UseVisualStyleBackColor = true;
             this.fromClipboardButton.Click += new System.EventHandler(this.fromClipboardButton_Click);
             // 
