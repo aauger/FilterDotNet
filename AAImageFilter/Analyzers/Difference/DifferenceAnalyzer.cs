80768c44cc3137fb88ddbd49d5daf402589e37af
Austin Auger <a.ustin.mark.auger@gmail.com>
2021-12-20 14:10:22 -0500

    Create DifferenceAnalyzer.cs
    Outline of difference analyzer

---
+++ AAImageFilter/Analyzers/Difference/DifferenceAnalyzer.cs
@@ -0,0 +1,48 @@
+﻿using AAImageFilter.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AAImageFilter.Analyzers
+{
+    public class DifferenceAnalyzer : IAnalyzer, IConfigurableAnalyzer
+    {
+        /* DI */
+        private readonly IAnalyzerConfigurator<IImage>? _analyzerConfigurator;
+
+        /* Internals */
+        private IImage? _other;
+        private bool _ready = false;
+
+        /* Properties */
+        public string Name => "Difference";
+
+        public string Analyze(IImage input)
+        {
+            int pxCount = input.Width * input.Height;
+            double invFrac = Math.Pow(pxCount, -1);
+
+            Parallel.For(0, input.Width, (int x) => 
+            {
+                Parallel.For(0, input.Height, (int y) => 
+                { 
+                    IColor here = input.GetPixel(x, y);
+                    IColor there = _other!.GetPixel(x, y);
+
+
+                });
+            });
+
+            return "";
+        }
+
+        public IAnalyzer Initialize()
+        {
+            _other = _analyzerConfigurator!.GetAnalyzerConfiguration();
+            _ready = true;
+            return this;
+        }
+    }
+}
