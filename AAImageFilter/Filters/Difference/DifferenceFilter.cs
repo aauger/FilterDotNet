68c7803dece9f6654d1dd3974b211d4d66363e33
Austin Auger <a.ustin.mark.auger@gmail.com>
2021-12-20 23:14:56 -0500

    Update DifferenceFilter.cs
    Add some bits & pieces for a difference filter.

--- AAImageFilter/Filters/Difference/DifferenceFilter.cs
+++ AAImageFilter/Filters/Difference/DifferenceFilter.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AAImageFilter.Interfaces;
 
 namespace AAImageFilter.Filters
 {
-    public class DifferenceFilter
+    public class DifferenceFilter : IFilter, IConfigurableFilter
     {
+        /* DI */
+
+        /* Internals */
+        private IImage _other = ;
+        private double _multiplier = 1.0;
+        
+        /* Properties */
+        public string Name => "Difference";
+
+        public IImage Apply(IImage input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IFilter Initialize()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
