e13bb0218ef29cde3b201fa40e6f76286115550d
Austin Auger <a.ustin.mark.auger@gmail.com>
2021-12-23 14:00:51 -0500

    Another cleanup pass on main filter library

--- AAImageFilter/LibraryConfigurators/LambdaGeneratorConfigurator.cs
+++ AAImageFilter/LibraryConfigurators/LambdaGeneratorConfigurator.cs
@@ -1,9 +1,4 @@
 ﻿using AAImageFilter.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AAImageFilter.LibraryConfigurators
 {
--- AAImageFilter/LibraryConfigurators/LambdaPluginConfigurator.cs
+++ AAImageFilter/LibraryConfigurators/LambdaPluginConfigurator.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AAImageFilter.Interfaces;
+﻿using AAImageFilter.Interfaces;
 
 namespace AAImageFilter.LibraryConfigurators
 {
