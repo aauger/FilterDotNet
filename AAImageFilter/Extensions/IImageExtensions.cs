08914b027b0fe5fd0d1f7477e703fc9b7fc05161
Austin Auger <a.ustin.mark.auger@gmail.com>
2022-01-06 22:02:15 -0500

    Additional prepwork for Melting filter

--- AAImageFilter/Extensions/IImageExtensions.cs
+++ AAImageFilter/Extensions/IImageExtensions.cs
@@ -5,5 +5,18 @@ namespace AAImageFilter.Extensions
     public static class IImageExtensions
     {
         public static bool OutOfBounds(this IImage image, int x, int y) => x < 0 || x >= image.Width || y < 0 || y >= image.Height;
+
+        public static IImage Clone(this IImage image, Func<int, int, IImage> imageCreator)
+        {
+            IImage output = imageCreator(image.Width, image.Height);
+            Parallel.For(0, image.Width, (int x) => 
+            {
+                Parallel.For(0, image.Height, (int y) => 
+                {
+                    output.SetPixel(x, y, image.GetPixel(x, y));
+                });
+            });
+            return output;
+        }
     }
 }
--- AAImageFilter/Filters/Melting/MeltingFilter.cs
+++ AAImageFilter/Filters/Melting/MeltingFilter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AAImageFilter.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,13 @@ using System.Threading.Tasks;
 
 namespace AAImageFilter.Filters
 {
-    public class MeltingFilter
+    public class MeltingFilter : IFilter
     {
+        public string Name => "Melting";
 
+        public IImage Apply(IImage input)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
