2ee81e38564e26006821e06e2997c22dc6bd7669
Austin Auger <a.ustin.mark.auger@gmail.com>
2022-01-13 22:29:27 -0500

    Create ClippingRectangleFilter.cs
    New stub code for ClippingRectangleFilter which will take another IFilter & run it against a clipped portion of the input, and then re-apply that over the clip-rect in the final output.
    
    i.e. running a filter over a 'selection' in other image processing tools

---
+++ FilterDotNet/ClippingRectangleFilter.cs
@@ -0,0 +1,75 @@
+﻿using FilterDotNet.Exceptions;
+using FilterDotNet.Extensions;
+using FilterDotNet.Interfaces;
+
+namespace FilterDotNet
+{
+    public class ClippingRectangle 
+    { 
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+
+    public class ClippingRectangleFilter : IFilter, IConfigurableFilter
+    {
+        /* DI */
+        private readonly IPluginConfigurator<(ClippingRectangle, IFilter)> _pluginConfigurator;
+        private readonly IEngine _engine;
+
+        /* Internals */
+        private bool _ready = false;
+        private IFilter? _filter;
+        private ClippingRectangle? _clip;
+
+        public ClippingRectangleFilter(IPluginConfigurator<(ClippingRectangle, IFilter)> pluginConfigurator, IEngine engine)
+        {
+            this._pluginConfigurator = pluginConfigurator;
+            this._engine = engine;
+        }
+
+        public string Name => "Clipping Rectangle Filter";
+
+        public IImage Apply(IImage input)
+        {
+            if (!this._ready)
+                throw new NotReadyException();
+
+            IImage output = input.Clone(this._engine.CreateImage);
+            IImage clippedRectImage = Clip(input);
+            IFilter filter = this._filter!;
+            if (filter is IConfigurableFilter icf)
+                icf.Initialize();
+            IImage clippedFilteredOutput = filter.Apply(clippedRectImage);
+            Parallel.For(0, this._clip!.Width, (int x) => 
+            {
+                Parallel.For(0, this._clip!.Height, (int y) => 
+                {
+                    throw new NotImplementedException();
+                });
+            });
+        }
+
+        private IImage Clip(IImage input)
+        {
+            IImage output = this._engine.CreateImage(this._clip!.Width, this._clip!.Height);
+            Parallel.For(0, this._clip.Width, (int x) => 
+            {
+                Parallel.For(0, this._clip.Height, (int y) => 
+                { 
+                    output.SetPixel(x+this._clip.X, y + this._clip.Y,
+                        input.GetPixel(x + this._clip.X, y + this._clip.Y));
+                });
+            });
+            return output;
+        }
+
+        public IFilter Initialize()
+        {
+            (this._clip, this._filter) = _pluginConfigurator.GetPluginConfiguration();
+            this._ready = true;
+            return this;
+        }
+    }
+}
