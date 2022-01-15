4047036e0a97aa56bf08438ea687c42fc2f2d68b
Austin Auger <a.ustin.mark.auger@gmail.com>
2022-01-14 22:27:58 -0500

    Create FloydSteinbergDitherFilter.cs
    Added stub for Floyd-Steinberg Dithering to be added.

---
+++ FilterDotNet/Filters/FloydSteinbergDither/FloydSteinbergDitherFilter.cs
@@ -0,0 +1,19 @@
+﻿using FilterDotNet.Interfaces;
+
+namespace FilterDotNet.Filters
+{
+    public class FloydSteinbergDitherFilter : IFilter, IConfigurableFilter
+    {
+        public string Name => "Floyd-Steinberg Dither";
+
+        public IImage Apply(IImage input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IFilter Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
