df3057d04e6d2da40a0db02af9de94684908a973
Austin Auger <a.ustin.mark.auger@gmail.com>
2022-01-18 16:14:53 -0500

    Update PatchMatchFilter.cs
    Using cleanups

--- FilterDotNet/Filters/PatchMatch/PatchMatchFilter.cs
+++ FilterDotNet/Filters/PatchMatch/PatchMatchFilter.cs
@@ -1,11 +1,6 @@
 ﻿using FilterDotNet.Exceptions;
 using FilterDotNet.Extensions;
 using FilterDotNet.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FilterDotNet.Filters
 {
