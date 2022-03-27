ce9be61387d055164c2fbe0bceabf99b6a60fe82
Austin Auger <a.ustin.mark.auger@gmail.com>
2022-03-26 22:16:07 -0400

    Add Channel Rotation filter
    this rotates the R, G, and B channel values in an image

---
+++ FilterDotNet/Filters/ColorFilters/RotateChannels/RotateChannelsFilter.cs
@@ -0,0 +1,40 @@
+﻿using FilterDotNet.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilterDotNet.Filters
+{
+    public class RotateChannelsFilter : IFilter
+    {
+        /* DI */
+        private readonly IEngine _engine;
+
+        /* Properties */
+        public string Name => "Rotate Channels";
+
+        public RotateChannelsFilter(IEngine engine)
+        {
+            this._engine = engine;
+        }
+
+        public IImage Apply(IImage input)
+        {
+            IImage output = this._engine.CreateImage(input.Width, input.Height);
+
+            Parallel.For(0, input.Width, (int x) =>
+            {
+                Parallel.For(0, input.Height, (int y) => 
+                {
+                    IColor original = input.GetPixel(x, y);
+                    IColor transformed = this._engine.CreateColor(original.G, original.B, original.R, original.A);
+                    output.SetPixel(x, y, transformed);
+                });
+            });
+
+            return output;
+        }
+    }
+}
