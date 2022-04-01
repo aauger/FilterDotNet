1bb8d782e9cbdec2d787e4c0e3a3929f7f4fd068
Austin Auger <a.ustin.mark.auger@gmail.com>
2022-04-01 15:19:03 -0400

    Introduce Optional project into repo

--- NET6ImageFilter/Program.cs
+++ NET6ImageFilter/Program.cs
@@ -7,14 +7,10 @@ using FilterDotNet.Analyzers;
 using FilterDotNet.LibraryConfigurators;
 using static FastImageProvider.Injectables;
 
-
 namespace NET6ImageFilter
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
---
+++ Optional/Option.cs
@@ -0,0 +1,45 @@
+﻿namespace Optional
+{
+    public struct Option<T>
+    {
+        private readonly bool HasValue = false;
+        private readonly T Value;
+
+        private Option(bool hasValue, T value)
+        {
+            this.HasValue = hasValue;
+            this.Value = value;
+        }
+
+        public static Option<T> Some(T item)
+        {
+            if (item is null)
+                return new(false, default);
+            else
+                return new(true, item);
+        }
+
+        public static Option<T> None => new(false, default);
+
+        public void Match(Action<T> some, Action none)
+        {
+            if (HasValue)
+                some(this.Value);
+            else
+                none();
+        }
+
+        public void Then(Action<T> some)
+        {
+            if (HasValue)
+                some(this.Value);
+        }
+
+        public Option<T> Or(T other)
+        {
+            if (!HasValue)
+                return Some(other);
+            return this;
+        }
+    }
+}
